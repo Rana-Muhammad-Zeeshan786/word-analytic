293d1763e2ef8c9825464296928df446068560a8
Rana-Muhammad-Zeeshan786 <zeeshan.dev66@gmail.com>
2024-03-31 12:25:44 +0500

    basic validation

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,5 +1,39 @@
+import { useState } from 'react'
+import Warning from './Warning'
+
 const Textarea = () => {
-  return <textarea className="textarea" />
+  const [text, setText] = useState('')
+  const [warningText, setWarningText] = useState('')
+
+  const onTextareChange = (e) => {
+    e.preventDefault()
+
+    let newText = e.target.value
+
+    if (newText.includes('<script>')) {
+      setWarningText('No script tag allowed')
+      newText = newText.replace('<script>', '')
+    } else if (newText.includes('@')) {
+      setWarningText('No @ symbol allowed')
+      newText = newText.replace('@', '')
+    } else {
+      setWarningText('')
+    }
+
+    setText(newText)
+  }
+
+  return (
+    <div className="textarea">
+      <textarea
+        value={text}
+        placeholder="Enter your text"
+        spellCheck="false"
+        onChange={onTextareChange}
+      />
+      <Warning warningText={warningText} />
+    </div>
+  )
 }
 
 export default Textarea
